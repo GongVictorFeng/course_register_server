13cfe8eceda0292fb4b146464857ac963fbc22af
victor <gfeng005@uottawa.ca>
2025-02-25 20:48:43 -0500

    updated the server

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ import { getAllCourses, getCourseById } from "./get-courses.route.js";
 import { searchLessons } from './search-lessons.route.js';
 
 const app = express();
+const port = process.env.PORT || 9000;
 
 app.route('/api/courses').get(getAllCourses);
 app.route('/api/courses/:id').get(getCourseById);
 app.route('/api/lessons').get(searchLessons);
 
-const httpServer = app.listen(9000, () => {
+const httpServer = app.listen(port, () => {
     console.log("HTTP REST API Server running at http://localhost:" + httpServer.address().port);
 });
